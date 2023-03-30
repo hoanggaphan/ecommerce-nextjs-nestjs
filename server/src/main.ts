bb56d9aee7529104d9f6529a899b83363c6fcc66
hoanggaphan <phansihoang1998@gmail.com>
2023-03-30 15:11:44 +0700

    fix deploy in production

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,10 @@ import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.NODE_ENV === 'development' ? '*' : process.env.CLIENT,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors({
+  //   origin: process.env.NODE_ENV === 'development' ? '*' : process.env.CLIENT,
+  // });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,4 +20,3 @@ async function bootstrap() {
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
-//
